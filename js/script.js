d92aad16c5aa4c9f00fde21ce3fec6c0df0f3fa0
Ampedeth <gav.fadt@gmail.com>
2023-11-14 17:09:24 +0100

    new code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+document.body.style.background = 'pink';
 
 //------------------------------//
 //------------------------------//
@@ -66,7 +67,7 @@
 
 // const student3 = {
 //     name: "Ivan",
-//     ocupation: "Soft Ingenner",
+//     ocupation: "Soft Engenner",
 //     middleMark: 10,
 //     missedLessons: 3,
 // }
@@ -245,3 +246,216 @@
 // }
 // console.log(res);
 
+//------------------------------//
+//------------------------------//
+
+// function wordSearch(query, seq){
+//     //your code here
+//     let res = [];
+//     for (let i = 0; i < seq.length; i++){
+//       const char = seq[i];
+//       if (char.toLowerCase().includes(query.toLowerCase())){
+//         res.push(char);
+//       }
+//     }
+//     if (res.length > 0){
+//         return console.log(res);
+//     } else{
+//         return console.log(["Empty"]);
+//     }
+    
+//   }
+// MyArray = ["dad", "adad", "potada", "dsada", "jkj"];
+// wordSearch("ad", MyArray);
+
+//------------------------------//
+//------------------------------//
+
+// class CircleBox {
+//     constructor(selector){
+//         this.$el = document.querySelector(selector);
+//     }
+
+//     hide(){
+//         this.$el.style.display = "none";
+//     }
+
+//     show(){
+//         this.$el.style.display = "block";}
+// }
+
+// class CircleItem extends CircleBox{
+//     constructor(options){
+//         super(options.selector)
+//         this.$el.style.width = options.size + "px";
+//         this.$el.style.height = options.size + "px";
+//         this.$el.style.borderRadius = "50%";
+//         this.$el.style.background = options.color;
+
+//     }
+// }
+
+// const CircleRed = new CircleItem({
+//     selector: "#circleRed",
+//     color: "red",
+//     size: 50,
+// });
+// const CircleBlue = new CircleItem({
+//     selector: "#circleBlue",
+//     color: "blue",
+//     size: 25,
+// });
+// const CircleOrange = new CircleItem({
+//     selector: "#circleOrange",
+//     color: "orange",
+//     size: 70,
+// });
+
+//------------------------------//
+//------------------------------//
+
+// function Car(name, color){
+//     this.name = name;
+//     this.color = color;
+// }
+
+// Car.prototype.message =function(){
+//     console.log(`${this.name}'s color is ${this.color}`)
+// }
+
+// const BMW = new Car('bmw', 'red');
+// const opel = new Car("opel", "white");
+
+//------------------------------//
+//------------------------------//
+
+// let message ={
+//     messageHello(){
+//         console.log(`Hello, ${this.name}`);
+//     },
+//     messageBye(){
+//         console.log(`Bye, ${this.name}`);
+//     },
+// };
+// class User {
+//     constructor(name){
+//         this.name = name;
+//     };
+// }
+// Object.assign(User.prototype, message);
+
+// new User("Ivan").messageHello();
+
+//------------------------------//
+//------------------------------//
+
+// function Calculator(){
+//     this.number1 = 0;
+//     this.number2 = 0;
+
+//     this.read = function(){
+//          this.number1 = Number(prompt('Type first number'));
+//          this.number2 = Number(prompt('Type second number'));
+//     };
+
+//     this.sum = function(){
+//         let sumRes = this.number1 + this.number2;
+//         console.log(sumRes);
+//         return sumRes;
+//     };
+    
+//     this.mul = function(){
+//         return this.number1 * this.number2;
+//     };
+// }
+// const MyCalculator = new Calculator();
+// MyCalculator.read();
+// console.log(`Summ: ${MyCalculator.sum()}`);
+// console.log(`Multiply: ${MyCalculator.mul()}`);
+
+
+//------------------------------//
+//------------------------------//
+
+//*******************************//
+//DOM stuff
+//*******************************//
+
+// create div
+const div = document.createElement("div");
+div.classList.add('wrapper');
+const body = document.querySelector('body');
+body.appendChild(div);
+//------------------------------//
+// create tittle H1"DOM..."
+const header = document.createElement('h1');
+header.textContent = "DOM (Document Object Model)";
+div.insertAdjacentElement('beforebegin', header);
+//------------------------------//
+//create list <ul></ul>
+const ul = `
+    <ul>
+        <li>one</li>
+        <li>two</li>
+        <li>three</li>
+    </ul>`;
+div.innerHTML = ul;
+//------------------------------//
+//create picture
+const img = document.createElement('img');
+img.src = 'https://picsum.photos/240';
+img.width = 240;
+img.classList.add('super');
+img.alt = 'Super Man'
+div.appendChild(img);
+//------------------------------//
+//using HTML line, create div with class 'pDiv' + with 2 paragraph
+const elemHTML = `
+<div class='pDiv'>
+    <p>paragraph 1</p>
+    <p>paragraph 2</p>
+</div>`
+const ulList = div.querySelector('ul');
+ulList.insertAdjacentHTML('beforebegin', elemHTML)
+
+const pDiv = document.querySelector('.pDiv');
+pDiv.children[1].classList.add('text');
+pDiv.children[0].remove(); 
+//------------------------------//
+// create function generateAutoCard, which get 3 arguments: brand, color, year
+const generateAutoCard = (brand, color, year) => {
+    const curDate = new Date();
+    const curYear = curDate.getFullYear();
+    return`
+    <div class="autoCard">
+    <h2>${brand.toUpperCase()} ${year}</h2>
+    <p>Auto ${brand.toUpperCase()} - ${year}. Auto's age - ${curYear - year}</p>
+    <p>Color is: ${color}</p>
+    <button type='button' class='btn'>delete</button>
+    </div>
+    `;
+}
+const carsDiv = document.createElement('div');
+carsDiv.classList.add('autos');
+
+const carsList = [
+    {brand: 'Tesla', year: 2015, color: 'red'},
+    {brand: 'Lexus', year: 2016, color: 'silver'},
+    {brand: 'Nissan', year: 2012, color: 'black'},
+]
+
+const carsHTML =  carsList.map(car=> {
+    return generateAutoCard(car.brand, car.color, car.year);
+}).join('');
+
+carsDiv.innerHTML = carsHTML;
+div.insertAdjacentElement('beforebegin', carsDiv);
+
+const buttons = document.querySelectorAll('.btn');
+function handleClick(e) {
+    const currentButton = e.currentTarget;
+    currentButton.closest('.autoCard').remove();
+}
+buttons.forEach(button => {
+    button.addEventListener('click', handleClick)
+})
